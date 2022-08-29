fa7f100f8043df6d649e7ce3f3fabde9c6bc9975
lakeside763 <lakeside763@gmail.com>
2022-08-29 08:04:39 +0100

    chore: validate authorization header

--- src/backend/src/middlewares/require-auth.ts
+++ src/backend/src/middlewares/require-auth.ts
@@ -4,6 +4,8 @@ import { AuthenticationError } from '../utils/errors/authentication-error';
 
 const requireAuth = async (req: Request, _res: Response, next: NextFunction) => {
   try {
+    const authorizationHeader = await services.token.checkAuthorizationHeader({ req });
+    if (!authorizationHeader) throw new AuthenticationError('Authorization header is required');
     const auth = await services.token.getAuth({ req, next });
     req.auth = auth;
     if (!auth) {
