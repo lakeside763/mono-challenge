c73c30f395dfc11a045e26082276fde0a14bd509
lakeside763 <lakeside763@gmail.com>
2022-08-31 07:23:53 +0100

    fix: authorization error

--- src/backend/src/utils/utils.ts
+++ src/backend/src/utils/utils.ts
@@ -2,9 +2,13 @@ import { Response, NextFunction } from "express";
 
 export const errorResponse = (error: any, res: Response, next: NextFunction) => {
   if (error.name) {
+    if (error.code === 11000) {
+      const [keyValue] = Object.entries(error.keyValue);
+      return res.status(400).json({ errors: [{ message: `Existing ${keyValue[0]} with ${keyValue[1]}`}]})
+    }
     return res.status(400).json({ errors: [{ message: error.message }]});
   }
-  next(error);
+  return next(error);
 };
 
 export const fetch = async (url: string, data: any) => {
