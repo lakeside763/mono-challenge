3192d77661eb26bbea0a5f76312653d2ca17179c
lakeside763 <lakeside763@gmail.com>
2022-09-01 10:52:49 +0100

    chore: update eslint error

--- src/frontend/src/hooks/use-link.ts
+++ src/frontend/src/hooks/use-link.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useMemo, useEffect } from 'react';
 import MonoConnect from '@mono.co/connect.js';
 import useAccount from './use-account';
